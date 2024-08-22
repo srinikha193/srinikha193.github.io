589a8782b94dd0d987997bda01f4555170ea4b21
Srinikha Renikunta <156345758+srinikha193@users.noreply.github.com>
2024-08-22 01:58:34 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -188,7 +188,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'fantasy',
+    defaultTheme: 'procyon',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
